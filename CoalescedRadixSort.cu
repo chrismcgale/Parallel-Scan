487dec957858232c2189c16863f0257ed477e0f8
Christopher McGale <56483395+chrismcgale@users.noreply.github.com>
2024-04-03 01:04:24 -0400

    Coalesced radix sort

---
+++ CoalescedRadixSort.cu
@@ -0,0 +1,92 @@
+
+// Idea behind radix sort is to iterate over each digit (left to right) and order
+// Not an in place algorithm
+__host__ radix_sort(unsigned int* input, unsigned int* output, unisgned int n, unsigned int sigFigs) {
+
+    for (int i = 0; i < sigFigs; i++) {
+
+        // 2 * num of blocks
+        unsigned int onesPerBlock[2*(n / SECTION_SIZE)] = {0};
+        unsigned int blockPositions[2*(n / SECTION_SIZE)] = {0};
+
+        cudaMalloc((void**)&input, sizeof(unsigned int));
+        cudaMalloc((void**)&output, sizeof(unsigned int));
+
+        radix_sort_iter_get_bits<<<n / SECTION_SIZE, SECTION_SIZE>>>(input, bits, key, n, i);
+
+        // exclusive scan on ones / zeros in each block. Could change to parallel but likely too few blocks for it to make a big difference
+        blockPositions[0] = 0;
+        for (unsigned int j = 0; j < n / SECTION_SIZE; j++) {
+            for (unsigned int k = 0; k < blockDim.x; k++) if (bits[j][k] == 1) { onesPerBlock[j] += 1; } else { onesPerBlock[j + (n / SECTION_SIZE)] += 1; }
+        }
+
+        for (unsigned int j = 1; j < n / SECTION_SIZE; j++) {
+            blockPositions[i] = blockPositions[i - 1] + onesPerBlock[i - 1];
+        }
+
+        radix_sort_iter_assign<<<n / SECTION_SIZE, SECTION_SIZE>>>(bits, output, blockPositions, n);
+
+        // Repeat with output the new input
+        input = output;
+
+        cudaFree(input);
+        cudaFree(output);
+    }
+
+}
+
+// Sorts input within each block
+__global__ void radix_sort_iter_get_bits(unsigned int* input unsigned int* bits, unisgned int n, unsigned int iter) {
+    unsigned int i = threadIdx.x;
+    __shared__ unsigned int bits[blockDim.x];
+    unsigned int bit, key;
+    if (i < n) {
+        key = input[i];
+        bit = (key >> iter) & 1;
+        bits[i] = bit;
+        atomicAdd(blockPositions[blockIdx + (bit == 0) ? blockIdx : 0], 1);
+    }
+
+    __syncthreads();
+
+
+    // 
+    __shared__ unsigned int exclsuiveScannedBits[blockDim.x];
+
+    if (i < n) {
+        for (unsigned int stride = 1; stride < blockDim.x; stride *= 2) {
+            __syncthreads();
+            float temp;
+            if (threadIdx.x >= stride) {
+                temp = bits[threadIdx.x] + bits[threadIdx.x - stride];
+            }
+            __syncthreads();
+            if (threadIdx.x >= stride) {
+                bits[threadIdx.x] = temp;
+            }
+        }
+        if (i < n) {
+            exclsuiveScannedBits[i] = bits[threadIdx.x];
+        }
+    }
+
+    __syncthreads();
+
+    if (i < n) {
+        unsigned int numOnesBefore = (i > 0) ? exclsuiveScannedBits[i - 1] : 0;
+        unsigned int numOnesTotal = exclsuiveScannedBits[n - 1];
+        unsigned int dst = (bit == 0) ? (i - numOnesBefore) : (n - numOnesTotal - numOnesBefore);
+        
+        input[dst] = key;
+    }
+}
+
+// Global sort
+__global__ void radix_sort_iter_assign(unsigned int* input, unsigned int* output, unsigned int* blockPositions, unisgned int n) {
+    unsigned int i = threadIdx.x;
+    if (i < n) {
+        unsigned int dst = (bit == 0) ? (blockPositions[blockIdx.x + (n / SECTION_SIZE)] + i) : (blockPositions[blockIdx.x.x] + i);
+        
+        output[dst] = input[i];
+    }
+}
--- RadixSort.cu
+++ RadixSort.cu
@@ -6,19 +6,18 @@ __host__ radix_sort(unsigned int* input, unsigned int* output, unisgned int n, u
 
         unsigned int bits[n];
 
-        unsigned int key;
+        unsigned int keys[n];
 
         cudaMalloc((void**)&input, sizeof(unsigned int));
         cudaMalloc((void**)&output, sizeof(unsigned int));
         cudaMalloc((void**)&bits, sizeof(unsigned int));
-        cudaMalloc((void**)&&key, sizeof(unsigned int));
 
-        radix_sort_iter_get_bits<<<n / SECTION_SIZE, SECTION_SIZE>>>(input, output, bits, &key, n, i);
+        radix_sort_iter_get_bits<<<n / SECTION_SIZE, SECTION_SIZE>>>(input, output, bits, n, i);
 
         // exclusive scan on significant bits
         hierchical_scan(bits, bits, n, false);
 
-        radix_sort_iter_assign<<<n / SECTION_SIZE, SECTION_SIZE>>>(bits, output, bits, key, n);
+        radix_sort_iter_assign<<<n / SECTION_SIZE, SECTION_SIZE>>>(bits, output, bits, n);
 
         // Repeat with output the new input
         input = output;
@@ -26,7 +25,6 @@ __host__ radix_sort(unsigned int* input, unsigned int* output, unisgned int n, u
         cudaFree(input);
         cudaFree(output);
         cudaFree(bits);
-        cudaFree(key);
     }
 
 }
@@ -34,21 +32,22 @@ __host__ radix_sort(unsigned int* input, unsigned int* output, unisgned int n, u
 
 __global__ void radix_sort_iter_get_bits(unsigned int* input, unsigned int* output, unsigned int* bits, unsigned int* key, unisgned int n, unsigned int iter) {
     unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
-    unsigned int  bit;
+    unsigned int  bit, key;
     if (i < n) {
-        *key = input[i];
-        bit = (key >> iter) & 1;
+        key = input[i];
+        bit = (key[i] >> iter) & 1;
         bits[i] = bit;
     }
+    // Alt approach is to call an exclusive scan on bits here and then go right into radix_sort_iter_assign
 }
 
-__global__ void radix_sort_iter_assign(unsigned int* input, unsigned int* output, unsigned int* bits, unsigned int key, unisgned int n) {
+__global__ void radix_sort_iter_assign(unsigned int* input, unsigned int* output, unsigned int* bits, unisgned int n) {
     unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
     if (i < n) {
         unsigned int numOnesBefore = bits[i];
         unsigned int numOnesTotal = bits[n];
-        unsigned int dst = (bit == 0) ? (i - numOnesBefore) : (n - numOnesBefore - numOnesBefore);
+        unsigned int dst = (bit == 0) ? (i - numOnesBefore) : (n - numOnesTotal - numOnesBefore);
         
-        output[dst] = key;
+        output[dst] = input[i];
     }
 }
